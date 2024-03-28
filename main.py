a52a5e521ad73a7bf5ec51ff76d5b4bd1026f496
marceufilho <filhomarceu@gmail.com>
2024-03-28 18:14:39 -0300

    my solution to the bookbot

--- main.py
+++ main.py
@@ -45,7 +45,3 @@ def main():
 
 main()
 
-# count_char("""HIS eBook is for the use of anyone anywhere at no cost and with
-# almost no restrictions whatsoever.  You may copy it, give it away or
-# re-use it under the terms of the Project Gutenberg License included
-# with this eBook or online at www.gutenberg.net""")
